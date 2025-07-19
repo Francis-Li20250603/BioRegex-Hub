47af9165c17a343481e1cccf060635904ea6c223
HAPPY Mondschein ZJL <140026952+MondscheinDativ@users.noreply.github.com>
2025-07-19 15:56:38 +0100

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # 从环境变量获取数据库URL，或使用默认值
