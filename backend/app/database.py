39c6d12d27bf9087378cff0df366bd0ab04af75f
Francis-Li20250603 <liben19960927@163.com>
2025-09-04 20:19:36 +0800

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,4 @@
-frfrom sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine
 from app.core.config import settings
 
 engine = create_engine(settings.DATABASE_URL, echo=False)
